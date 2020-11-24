c588f8a969e027a905ba6e3ca2dca8a01b289abb
Orest <Slowyjkee@gmail.com>
2020-11-24 14:51:59 +0300

    remote changes

--- build/index.js
+++ build/index.js
@@ -21,54 +21,74 @@ function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try
 
 function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 
-var http = require('http');
+var server = require('http').Server(_index["default"]);
 
-var _process$env = process.env,
-    PORT = _process$env.PORT,
-    DB_CONNECTION = _process$env.DB_CONNECTION;
-var server = http.createServer(_index["default"]);
+var DB_CONNECTION = process.env.DB_CONNECTION;
+var port = process.env.PORT || 5000;
 var io = (0, _socket["default"])(server);
 exports.io = io;
 var clients = new Map();
 exports.clients = clients;
+server.listen(port || 3000, function () {
+  return console.log("trash ".concat(port, " port"));
+});
 io.on('connection', /*#__PURE__*/function () {
-  var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(socket) {
+  var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2(socket) {
     var id, user;
-    return regeneratorRuntime.wrap(function _callee$(_context) {
+    return regeneratorRuntime.wrap(function _callee2$(_context2) {
       while (1) {
-        switch (_context.prev = _context.next) {
+        switch (_context2.prev = _context2.next) {
           case 0:
             id = socket.handshake.query.id;
             clients.set(id, {
               socketID: socket.id
             });
+            console.log(clients);
 
             _index["default"].set('socket', socket);
 
             _index["default"].set('io', io);
 
-            _context.next = 6;
+            _context2.next = 7;
             return _user["default"].findByIdAndUpdate(id, {
               online: true
             });
 
-          case 6:
-            user = _context.sent;
-            socket.on('disconnect', function (callback) {
-              clients["delete"](socket.handshake.query.id);
+          case 7:
+            user = _context2.sent;
+            socket.on('disconnect', /*#__PURE__*/function () {
+              var _ref2 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(callback) {
+                return regeneratorRuntime.wrap(function _callee$(_context) {
+                  while (1) {
+                    switch (_context.prev = _context.next) {
+                      case 0:
+                        console.log(callback);
+                        clients["delete"](socket.handshake.query.id);
+                        _context.next = 4;
+                        return _user["default"].findByIdAndUpdate(id, {
+                          online: false,
+                          lastSeen: Date.now()
+                        });
 
-              _user["default"].findByIdAndUpdate(id, {
-                online: false,
-                lastSeen: new Date()
-              });
-            });
+                      case 4:
+                      case "end":
+                        return _context.stop();
+                    }
+                  }
+                }, _callee);
+              }));
 
-          case 8:
+              return function (_x2) {
+                return _ref2.apply(this, arguments);
+              };
+            }());
+
+          case 9:
           case "end":
-            return _context.stop();
+            return _context2.stop();
         }
       }
-    }, _callee);
+    }, _callee2);
   }));
 
   return function (_x) {
@@ -84,9 +104,5 @@ _mongoose["default"].connect(DB_CONNECTION, {
   return console.log('u are connected to DB');
 })["catch"](function (e) {
   return console.log(e);
-});
-
-server.listen(PORT, function () {
-  return console.log("u are listening to ".concat(PORT, " port"));
 });
 //# sourceMappingURL=index.js.map
