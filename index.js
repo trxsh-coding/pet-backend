c15e66d1373eb800ccaa5e9f00875a84ffb334a9
Орест Козель <kozel@myservices.digitil>
2021-03-16 21:11:20 +0300

    Cors fix

--- index.js
+++ index.js
@@ -30,6 +30,16 @@ app.use('/api/v1/notification', notificationRoute);
 app.use('/api/v1/like', likeRoute);
 app.use('/api/v1/missing', missingRoute);
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    next();
+});
+
 app.get('/',(req,res) => {
     res.send("Hello Babel")
 });
