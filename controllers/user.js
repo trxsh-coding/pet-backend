639287e69dc4c5ed7c9c7735bc8c7ca6ce8becff
Орест Козель <kozel@myservices.digitil>
2021-03-16 21:07:00 +0300

    -changed origin for cors

--- controllers/user.js
+++ controllers/user.js
@@ -58,9 +58,9 @@ const createSendToken = (user, statusCode, req, res) => {
     const token = signToken(user._id);
     const cookieOptions =  {
         expires:  new Date(Date.now() + process.env.ACCESS_TOKEN_EXPIRES * 24 * 60 * 1000),
-        // httpOnly:true,
+        httpOnly:true,
         sameSite: 'none',
-        // secure:true
+        secure:true
     };
     res.cookie('jwt', token, cookieOptions);
 
--- index.js
+++ index.js
@@ -14,10 +14,7 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import 'dotenv/config';
 const app = express();
-app.use(cors({
-    credentials: true,
-    origin: '*'
-}));
+app.use(cors());
 
 
 
