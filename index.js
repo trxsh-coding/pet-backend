bc01aefb8df32db7a0842c2d65583d755052c721
Орест Козель <kozel@myservices.digitil>
2021-03-17 16:10:26 +0300

    cors fix

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(cors({
 }));
 app.use((req, res, next) => {
     res.header('Access-Control-Expose-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "https://pethouse.caet");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
