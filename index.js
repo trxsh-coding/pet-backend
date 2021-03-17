736553e261baa689b170557f904530eef29e1718
Орест Козель <kozel@myservices.digitil>
2021-03-17 13:57:19 +0300

    cors fix

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ app.use(cors({
 app.use((req, res, next) => {
     res.header('Access-Control-Expose-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     const err = new Error('Not Found');
