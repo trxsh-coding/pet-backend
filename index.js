78964103a9d32bbe662a024cc0b405bde77d4ffb
Орест Козель <kozel@myservices.digitil>
2021-03-17 15:17:18 +0300

    cors fix

--- index.js
+++ index.js
@@ -18,15 +18,8 @@ import 'dotenv/config';
 const app = express();
 app.use(cors({
     credentials: true,
-    origin: ["http://localhost:3000", "https://www.pethouse.cat", "https://pethouse.cat"]
+    origin: ["http://localhost:3002", "https://www.pethouse.cat", "https://pethouse.cat"]
 }));
-app.use((req, res, next) => {
-    res.header('Access-Control-Expose-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next(err);
-})
 
 app.use(bodyParser.json());
 app.use(cookieParser());
@@ -41,11 +34,21 @@ app.use('/api/v1/like', likeRoute);
 app.use('/api/v1/missing', missingRoute);
 app.use('/api/v1/bookmark', bookmarkRoute);
 
+
+
+
 app.get('/',(req,res) => {
     res.send("Hello Babel")
 });
-app.use(errorController);
+app.use((req, res, next) => {
+    res.header('Access-Control-Expose-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header("Access-Control-Allow-Origin", "http://localhost:3002");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
 
+app.use(errorController);
 
 
 
