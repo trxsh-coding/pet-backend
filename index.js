a574429d57e93d6732cdfe2b13306faa246bcc47
Орест Козель <kozel@myservices.digitil>
2021-03-18 10:58:12 +0300

    cors fix

--- index.js
+++ index.js
@@ -22,6 +22,13 @@ app.use(cors({
     allowedHeaders: [ 'Accept-Version', 'Authorization', 'Credentials', 'Content-Type' ]
 }));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(`${__dirname}/public`));
