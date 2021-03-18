4376af465edd0304a0b0dbcd107e7e6c512588f4
Орест Козель <kozel@myservices.digitil>
2021-03-18 12:25:43 +0300

    cors fix

--- index.js
+++ index.js
@@ -21,16 +21,16 @@ app.set('trust proxy', '94.228.112.37');
 
 app.use(cors({
     credentials: true,
-    origin: ["http://localhost:3000", "https://www.pethouse.cat", "https://pethouse.cat"],
+    origin: ["http://localhost:3000", "www.pethouse.cat", "https://pethouse.cat"],
     allowedHeaders: [ 'Accept-Version', 'Authorization', 'Credentials', 'Content-Type' ]
 }));
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://pethouse.cat");
-    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "https://pethouse.cat");
+//     res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 app.use(bodyParser.json());
 app.use(cookieParser());
