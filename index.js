ea21bdd746a45da26386b92ad989171dea2d8fb0
Орест Козель <kozel@myservices.digitil>
2021-03-18 11:32:56 +0300

    cors fix

--- index.js
+++ index.js
@@ -16,18 +16,18 @@ import cors from 'cors';
 import session from 'express-session'
 import 'dotenv/config';
 const app = express();
-app.use(cors({
-    credentials: true,
-    origin: ["http://localhost:3000", "https://www.pethouse.cat", "https://pethouse.cat"],
-    allowedHeaders: [ 'Accept-Version', 'Authorization', 'Credentials', 'Content-Type' ]
-}));
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://pethouse.cat");
-    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use(cors({
+//     credentials: true,
+//     origin: ["http://localhost:3000", "https://www.pethouse.cat", "https://pethouse.cat"],
+//     allowedHeaders: [ 'Accept-Version', 'Authorization', 'Credentials', 'Content-Type' ]
+// }));
+//
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "https://pethouse.cat");
+//     res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 app.use(bodyParser.json());
 app.use(cookieParser());
