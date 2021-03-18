e4d82b7eac5910f764337163d6e75c924dc24a04
Орест Козель <kozel@myservices.digitil>
2021-03-18 10:28:50 +0300

    cors fix

--- index.js
+++ index.js
@@ -16,17 +16,10 @@ import cors from 'cors';
 import session from 'express-session'
 import 'dotenv/config';
 const app = express();
-// app.use(cors({
-//     credentials: true,
-//     origin: ["http://localhost:3000", "https://www.pethouse.cat", "https://pethouse.cat"]
-// }));
-app.use((req, res, next) => {
-    res.header('Access-Control-Expose-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header("Access-Control-Allow-Origin", "https://pethouse.cat");
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-})
+app.use(cors({
+    credentials: true,
+}));
+
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(`${__dirname}/public`));
