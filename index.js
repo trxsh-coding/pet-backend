b961d4d2a428ecab32a013a400fbf08b0eeb7663
Орест <Kozel@myservices.digital>
2021-04-29 18:19:05 +0300

    Fixes

--- index.js
+++ index.js
@@ -36,6 +36,14 @@ app.use('/api/v1/bookmark', bookmarkRoute);
 app.get('/',(req,res) => {
     res.send("Pethouse welcome!")
 });
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000, https://pethouse.cat');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 app.use(errorController);
 
 export default app;
